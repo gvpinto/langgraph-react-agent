7771c22c64a6b1b8c94c4d8644d3a11102780bed
Glenn Pinto <gvpinto@gmail.com>
2025-09-27 10:13:45 -0400

    Initial setup

---
+++ main.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+def async_main():
+    print('Hello ReAct langgraph with function calling!')
+
+
+if __name__ == "__main__":
+   async_main()
